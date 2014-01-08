a17f184c5dcc33fe0d326f999621fd30ad3ff624
Toshiyuki Nakamura <yukidarake4@gmail.com>
2014-01-08 14:56:21 +0900

    Rename

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -1,7 +1,7 @@
-class VimKaoriya < Cask
+class MacVimKaoriya < Cask
   url 'https://github.com/splhack/macvim/releases/download/20140106/macvim-kaoriya-20140106.dmg'
   homepage 'https://code.google.com/p/macvim-kaoriya/'
   version '20140106'
   sha1 '4745123f849de8819a44961115a3541577f2485e'
-  link 'MacVimTest.app'
+  link 'MacVim.app'
 end
